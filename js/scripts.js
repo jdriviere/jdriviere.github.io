3fa475191b65efa2162353a19f60665a62713687
unknown <getsparkle.css@gmail.com>
2018-08-29 23:57:00 -0600

    chore(index.html): Redesigned portfolio website.

--- js/scripts.js
+++ js/scripts.js
@@ -24,17 +24,13 @@ function setBindings() {
 		e.preventDefault ();
 		var sectionID = e.currentTarget.id + "Sec";
 
-		$("html body").animate ({
-			scrollTop: $("#" + sectionID).offset().top},
-			1000);
+		$("html body").animate ({ scrollTop: $("#" + sectionID).offset().top }, 1000);
 	});
 
 	$(".go-down i").click(function(e) {
 		e.preventDefault ();
 		var portfolioID = "folioSec";
 
-		$("html body").animate ({
-			scrollTop: $("#" + portfolioID).offset().top},
-			1000);
+		$("html body").animate ({ scrollTop: $("#" + portfolioID).offset().top }, 1000);
 	});
 } // End of setBindings
